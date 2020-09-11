e50a0210ddcd1b14900e50a8a7833a08445721f1
yash <yash.bonde999@gmail.com>
2020-09-11 12:03:19 -0400

    Created simple NodeJS server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>Hello World</p></body></html>");
+    res.end();
+  }
+});
+
+server.listen(8080);
